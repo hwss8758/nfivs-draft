f242bb2ea08217fe04f8f104264bd45501ed085f
wonsang.hwang <wonsang.hwang@navercorp.com>
2020-07-20 16:54:35 +0900

    프로젝트 생성
    초기 프로젝트 생성

---
+++ src/main/kotlin/com/nfivs/draft/DraftApplication.kt
@@ -0,0 +1,11 @@
+package com.nfivs.draft
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DraftApplication
+
+fun main(args: Array<String>) {
+    runApplication<DraftApplication>(*args)
+}
---
+++ src/test/kotlin/com/nfivs/draft/DraftApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.nfivs.draft
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DraftApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
